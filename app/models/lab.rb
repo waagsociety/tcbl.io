09768c2e2351f59bfe6b93e0218cd5b45d2ff40d
hiromipaw <silvia@nopressure.co.uk>
2015-12-10 12:45:05 +0100

    referee approval process sketch

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -48,6 +48,7 @@ class Lab < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
   belongs_to :referee, class_name: 'Lab'
   has_one :criteria, class_name: 'LabCriteria'
+  belongs_to :refere_approval_process
 
   validates_presence_of :name, :country_code, :slug#, :creator
   validates_presence_of :address_1, :kind, on: :create
