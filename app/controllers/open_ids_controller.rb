9146e508a5544e5cde28a3ef5362ac76f9cb7005
Taco van Dijk <taco@waag.org>
2017-04-11 13:39:19 +0200

    removed log

--- app/controllers/open_ids_controller.rb
+++ app/controllers/open_ids_controller.rb
@@ -8,12 +8,11 @@ class OpenIdsController < ApplicationController
 			params[:code],
 			stored_nonce
 		)
-		
+	
+		#TODO: bind the open_id to a user (same as account in example application)	
 		userinfo = open_id.to_access_token.userinfo!
-		puts "#{userinfo.email} logged in via sso"
 	
 		#if we have a user with the same email adress, lets log in
-		#TODO: this should work differently, by coupling it to an account or something	
         user = User.where(["email = ?", userinfo.email]).first
 		if(user != nil)
         	session[:user_id] = user.id
