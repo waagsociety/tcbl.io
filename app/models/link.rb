0dad44807034cc69ff863661dedf3d4782f12d25
Taco <taco@Tacos-MacBook-Pro-2.local>
2017-06-09 17:14:32 +0200

    fixed url validator

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,17 +1,35 @@
+class HttpUrlValidator < ActiveModel::EachValidator
+
+	def self.compliant?(value)
+		uri = URI.parse(value)
+		uri.is_a?(URI::HTTP) && !uri.host.nil?
+	rescue URI::InvalidURIError
+		false
+	end
+
+	def validate_each(record, attribute, value)
+		unless value.present? && self.class.compliant?(value)
+			record.errors.add(attribute, "is not a valid HTTP URL")
+		end
+	end
+
+end
+
 class Link < ActiveRecord::Base
-  belongs_to :linkable, polymorphic: true, touch: true
-  # validates_presence_of :lab
-  validates_presence_of :url
-  validates_uniqueness_of :url, scope: [:linkable_id, :linkable_type]
-  validates_format_of :url, with: URI::regexp(%w(http https))
-  before_validation :add_http
+	belongs_to :linkable, polymorphic: true, touch: true
+	# validates_presence_of :lab
+	validates_presence_of :url
+	validates_uniqueness_of :url, scope: [:linkable_id, :linkable_type]
+	validates :url, http_url: true
+	#validates_format_of :url, with: URI::regexp(%w(http https))
+	before_validation :add_http
 
-  scope :twitter_urls, -> { where("url ~* 'twitter.com/'").map(&:url) }
+	scope :twitter_urls, -> { where("url ~* 'twitter.com/'").map(&:url) }
 
-private
+	private
 
-  def add_http
-    self.url = "http://#{url}" if url.present? and !url.match(/^http/) and !url.match(/^https/)
-  end
+	def add_http
+		self.url = "http://#{url}" if url.present? and !url.match(/^http/) and !url.match(/^https/)
+	end
 
 end
