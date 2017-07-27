9ab4beee56005d579bf4276a6229ed7445ec78fc
Taco <taco@Tacos-MacBook-Pro-2.local>
2017-07-27 11:16:06 +0200

    sso register new user

--- app/controllers/open_ids_controller.rb
+++ app/controllers/open_ids_controller.rb
@@ -8,17 +8,53 @@ class OpenIdsController < ApplicationController
 			params[:code],
 			stored_nonce
 		)
-	
-		#TODO: bind the open_id to a user (same as account in example application)	
-		userinfo = open_id.to_access_token.userinfo!
-	
+
+		token = open_id.to_access_token
+		userinfo = token.userinfo!
+		#logger.info "userinfo: #{userinfo.inspect}"
+
+		#match by email 
 		#if we have a user with the same email adress, lets log in
-        user = User.where(["email = ?", userinfo.email]).first
+		#TODO (optional?): match by sub, if we can't find by sub, try to match by email, then assign sub... 
+		# not really necessary though, because email is the main login, 
+		# and if the gluu database needs to be reset, this can be used regardless..
+
+		user = User.where(["email = ?", userinfo.email]).first
 		if(user != nil)
-        	session[:user_id] = user.id
+			#actually perform the log in of the matched user
+			session[:user_id] = user.id
+			redirect_to root_url, flash: { success: "Signed in!" }
+			#logger.info("matched")
+		else
+			#generate random password
+			pw = (0...8).map { (65 + rand(26)).chr }.join
+
+			#register the user as a new user and then log in the user
+			#username is invalid
+			params = {
+				:email => userinfo.email,
+				:first_name => userinfo.given_name,
+				:last_name => userinfo.family_name,
+				:username => userinfo.email,
+				:password => pw,
+				:password_confirmation => pw
+			}
+			user = User.new params
+			#logger.info("created new user: #{user}")
+
+			#TODO: we need to remove the username validation, or change it so that it can also be an email address
+			#otherwise we can't update the user profile
+			if user.save(:validate => false) #skipping validation because we are creating the user programatically
+				#logger.info("saved new user")
+				UserMailer.delay.welcome(user.id)
+				session[:user_id] = user.id
+				redirect_to root_url, flash: { success: "Thanks for signing up. Please check your email to complete your registration." }
+			else
+				#logger.info user.errors.full_messages
+				redirect_to root_url, flash: { error: "Save new user failed..."}
+			end
 		end
-		
-		redirect_to root_url	
+
 	end
 
 	def create
